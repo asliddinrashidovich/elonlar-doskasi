b357535404e30059dae0f0ab3bb6831b6c3f4a73
Asliddin <asliddinrashidovich7@gmail.com>
2025-10-03 17:37:02 +0500

    express module is finished

--- controllers/postersController.js
+++ controllers/postersController.js
@@ -1,7 +1,45 @@
-const posterController = (req, res) => {
+const { v4 } = require("uuid")
+const { addNewPosterToDB, getAllPosters, getPosterByID, editPosterBydId, deletePosterById } = require("../db/posters")
+
+const posterController = async (req, res) => {
+    const posters = await getAllPosters()
     res.render("posters/posters", {
         title: "posters",
-        url: process.env.URL
+        url: process.env.URL,
+        posters
+    })
+}
+
+const getOnePoster = async (req, res) => {
+    const poster = await getPosterByID(req.params.id)
+    res.render("posters/one", {
+        title: poster.title,
+        poster
+    })
+}
+
+const updatePoster = async (req, res) => {
+    const editedPoster = {
+        title: req.body.title,
+        amount: req.body.amount,
+        image: req.body.image,
+        region: req.body.region,
+        description: req.body.description,
+    }
+    await editPosterBydId(req.params.id, editedPoster)
+    res.redirect("/posters")
+}
+
+const deletePoster = async (req, res) => {
+    await deletePosterById(req.params.id)
+    res.redirect("/posters")
+}
+
+const getEditPosterPage = async (req, res) => {
+    const poster = await getPosterByID(req.params.id)
+    res.render("posters/edit-poster", {
+        title: "Edit poster",
+        poster
     })
 }
 
@@ -12,13 +50,26 @@ const addPosterController = (req, res) => {
     })
 }
 
-const addNewPosterController = (req, res) => {
-    console.log(req.body);
+const addNewPosterController = async (req, res) => {
+    const poster = {
+        id: v4(),
+        title: req.body.title,
+        amount: req.body.amount,
+        region: req.body.region,
+        image: req.body.image,
+        description: req.body.description
+    }
+    await addNewPosterToDB(poster)
+    res.redirect("/")
 }
 
 
 module.exports = {
     posterController,
     addPosterController,
-    addNewPosterController
+    addNewPosterController,
+    getOnePoster,
+    getEditPosterPage,
+    updatePoster,
+    deletePoster
 }
--- routes/postersRoutes.js
+++ routes/postersRoutes.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
-const { posterController, addPosterController, addNewPosterController } = require("../controllers/postersController");
+const { posterController, addPosterController, addNewPosterController, getOnePoster, getEditPosterPage, updatePoster, deletePoster } = require("../controllers/postersController");
 const router = Router();
 
 router.get("/", posterController)
 router.get("/add", addPosterController)
 router.post("/add", addNewPosterController)
+router.get("/:id", getOnePoster)
+router.get("/:id/edit", getEditPosterPage)
+router.post("/:id/edit", updatePoster)
+router.post("/:id/delete", deletePoster)
 
 module.exports = router;
